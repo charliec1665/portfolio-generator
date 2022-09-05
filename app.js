baf1e5a5ebe662733dba9e0b6b25a73776aeac64
Charlie Carter <charliec1665@gmail.com>
2022-09-05 14:53:28 -0400

    added generate basic html

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
 
-    console.log('===================');
+const [name, github] = profileDataArgs;
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error (err);
 
-
-
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
