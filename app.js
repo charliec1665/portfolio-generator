4cc19d02c0c795cf81d0026dbac6e74dae0e6417
Charlie Carter <charliec1665@gmail.com>
2022-09-07 13:22:48 -0400

    cleaned up promise chain

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -180,16 +180,27 @@ const mockData = {
 
 // const pageHTML = generatePage(mockData); // dont forget to change this back to the function below
 
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+// });
+
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('./index.html', pageHTML, err => {
-            if (err) throw err;
-
-            console.log('Portfolio complete! Check out index.html to see the output!');
-        });
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
 
 
